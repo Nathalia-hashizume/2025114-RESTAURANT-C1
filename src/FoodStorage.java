57c3d21fa3feaae57d8506862ed3bcda73b98fb6
nathaliahashizume <nathaliahashizume@hotmail.com>
2025-10-22 15:44:01 +0100

    2 - I implemented the main logic of the FoodStorage class

--- src/FoodStorage.java
+++ src/FoodStorage.java
@@ -1,1 +1,76 @@
-
+//Class for our storage
+public class FoodStorage {
+    private FoodItem[] items;
+    private final int CAPACITY = 8; // The Capacity is 8
+    private int top;
+    private int front;
+    private int rear;
+    private boolean useOppositeSide;
+    
+    public FoodStorage(boolean useOppositeSide) {
+        this.items = new FoodItem[CAPACITY];
+        this.useOppositeSide = useOppositeSide;
+        this.top = -1;
+        this.front = -1;
+        this.rear = -1;
+    }
+    
+    public void addItem(FoodItem item) {
+        if (useOppositeSide) {
+            if (rear == CAPACITY -1) {
+                System.out.println("ERROR: Full Storage");
+                return;
+            }
+            if (front ==-1) front = 0;
+            items[++rear] = item;
+            System.out.println("Add (FIFO):" + item.toString());
+        } else {
+            if (top == CAPACITY -1) {
+                System.out.println("ERROR: Full Storage");
+                return;
+            }
+            items[++top] = item;
+            System.out.println("Add (LIFO):" + item.toString());
+        }
+    }
+    
+    public void removeItem() {
+        if (useOppositeSide){
+            if (front ==-1 || front > rear) {
+                System.out.println("ERROR: Empty Storage");
+                return;
+            }
+            FoodItem removed = items[front++];
+            System.out.println("REMOVED (FIFO): " + removed.toString());
+        } else {
+            if (top ==-1){
+                System.out.println("ERROR: Empty Storage");
+                return;
+            }
+            FoodItem removed = items[top--];
+            System.out.println("REMOVED (LIFO): " + removed.toString());
+        }
+    }
+    
+    public void displayItems() {
+        System.out.println("--- items in storage ---");
+        if (useOppositeSide) {
+            if (front ==-1 || front > rear) {
+                System.out.println("Empty");
+                return;
+            }
+            for (int i = front; i <= rear; i++) {
+                System.out.println(items[i].toString());
+            }
+        } else {
+            if (top ==-1) {
+                System.out.println("Empty");
+                return;
+            }
+            for (int i =0; i<= top; i++) {
+                System.out.println(items [i].toString());
+            }
+        }
+        System.out.println("-------------------------");
+    }
+}
